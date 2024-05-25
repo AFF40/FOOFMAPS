15257c7e8495940bb82bfeb5157e834abc86f450
AFF40 <alejandro.fernando.flores99@gmail.com>
2024-05-24 23:57:18 -0400

    agregado el campo de celular y quitado el de email al registro.java

--- app/src/main/java/com/example/foofmaps/clientes/restaurantes/RegisterRequest.java
+++ app/src/main/java/com/example/foofmaps/clientes/restaurantes/RegisterRequest.java
@@ -11,20 +11,21 @@ import java.util.Map;
 
 public class RegisterRequest extends StringRequest {
 
-    //conexion con el archivo php
-    private static final String controladorURL = Config.CONTROLADOR_URL+"controladorPersona.php";
-    private final Map<String,String>params;
-    public RegisterRequest(String username, String email, String pass1, String pass2, Response.Listener<String> listener){
-        super(Method.POST, controladorURL,listener,null);
-        params=new HashMap<>();
-        Log.e("info",username);
-        Log.e("info",email);
-        params.put("username",username);
-        params.put("email",email);
-        params.put("pass1",pass1);
-        params.put("pass2",pass2);
+    // Conexión con el archivo PHP
+    private static final String controladorURL = Config.CONTROLADOR_URL + "controladorPersona.php";
+    private final Map<String, String> params;
 
+    public RegisterRequest(String username, String celular, String pass1,String pass2, Response.Listener<String> listener) {
+        super(Method.POST, controladorURL, listener, null);
+        params = new HashMap<>();
+        Log.e("info", username);
+        Log.e("info", celular);
+        params.put("username", username);
+        params.put("celular", celular);
+        params.put("pass1", pass1);
+        params.put("pass2", pass2);
     }
+
     @Override
     public Map<String, String> getParams() {
         return params;
--- app/src/main/java/com/example/foofmaps/clientes/restaurantes/registro.java
+++ app/src/main/java/com/example/foofmaps/clientes/restaurantes/registro.java
@@ -1,11 +1,14 @@
 package com.example.foofmaps.clientes.restaurantes;
-import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 import com.example.foofmaps.R;
@@ -14,7 +17,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 public class registro extends AppCompatActivity {
 
-    EditText ed_username, ed_email, ed_pass1, ed_pass2;
+    EditText ed_username, ed_celular, ed_pass1, ed_pass2;
         Button btnRegistrar;
 
     @Override
@@ -23,7 +26,7 @@ public class registro extends AppCompatActivity {
         setContentView(R.layout.activity_registro);
 
         ed_username = findViewById(R.id.ed_user);
-        ed_email = findViewById(R.id.ed_email);
+        ed_celular = findViewById(R.id.ed_celular);
         ed_pass1 = findViewById(R.id.ed_pass1);
         ed_pass2 = findViewById(R.id.ed_pass2);
         btnRegistrar = findViewById(R.id.btn_registrar);
@@ -31,12 +34,13 @@ public class registro extends AppCompatActivity {
         btnRegistrar.setOnClickListener(v -> {
             // Obtener los valores ingresados por el usuario
             String username = ed_username.getText().toString();
-            String email = ed_email.getText().toString();
+            String celular = ed_celular.getText().toString();
             String pass1 = ed_pass1.getText().toString();
             String pass2 = ed_pass2.getText().toString();
 
+
             // Realizar la solicitud al servidor
-            RegisterRequest registerRequest = new RegisterRequest(username, email, pass1, pass2,
+            RegisterRequest registerRequest = new RegisterRequest(username, celular, pass1,pass2,
                     response -> {
                         try {
                             JSONObject jsonResponse = new JSONObject(response);
@@ -49,9 +53,11 @@ public class registro extends AppCompatActivity {
                                 Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_SHORT).show();
                                 Intent intent_registro_exitoso = new Intent(registro.this, MainActivity.class);
                                 registro.this.startActivity(intent_registro_exitoso);
+                                finish();
                             } else {
                                 // Error en el registro, mostrar un mensaje al usuario
                                 Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_SHORT).show();
+                                Log.e("info",mensaje);
                             }
                         } catch (JSONException e) {
                             e.printStackTrace();
